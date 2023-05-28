2e58180541176c9dc816ea6b64c4690671c0e74b
Ilias Samathrakis <134223309+isamathr@users.noreply.github.com>
2023-05-29 01:39:01 +0200

    Add files via upload

---
+++ myfootballanalytics/__init__.py
@@ -0,0 +1,7 @@
+from .mfa import DataUpdater
+from .mfa import FindIDs
+from .mfa import MatchDataLoader
+from .mfa import MatchAnalyzer
+from .mfa import LeagueAnalyzer
+from .mfa import TeamDataLoader
+from .mfa import TeamAnalyzer
