445b107626e5b43659674ee4dcb4972725a6467c
Ilias Samathrakis <134223309+isamathr@users.noreply.github.com>
2023-05-29 01:35:49 +0200

    Delete src directory

--- src/myfootballanalytics/__init__.py
+++
@@ -1,7 +0,0 @@
-from .main import DataUpdater
-from .main import FindIDs
-from .main import MatchDataLoader
-from .main import MatchAnalyzer
-from .main import LeagueAnalyzer
-from .main import TeamDataLoader
-from .main import TeamAnalyzer
